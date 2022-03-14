63b83fd8a12849833f4f9b08d88eb14d1437b915
GetSetAbhi <araina97@gmail.com>
2022-03-14 19:22:16 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   locations: string[] = ['Downtown', 'S. County', 'Lakeside'];
 
-  constructor(){ }
+  myForm: FormGroup;
+  mySecondForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.myForm = new FormGroup({
+      name: new FormControl('Sammy'),
+      email: new FormControl(''),
+      message: new FormControl(''),
+      address: new FormGroup({
+        city: new FormControl(''),
+        street: new FormControl(''),
+        pincode: new FormControl(''),
+      }),
+    });
 
+    this.mySecondForm = this.fb.group({
+      name: ['Sammy', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(15)]],
+    });
+  }
+
+  onSubmit(form) {
+    console.log(form.get('address'));
+    console.log(form.value);
   }
 }
